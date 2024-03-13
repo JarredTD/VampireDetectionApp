889c0b73325d32db1ee5af8cb2faa8042f0b137b
JarredTD <jarred.desrosiers@yahoo.com>
2024-03-12 22:15:52 -0400

    feat: Add model logic for threshold and random decision

---
+++ scripts/model.js
@@ -0,0 +1,18 @@
+class VampireModel {
+    constructor() {}
+
+    thresholdDecision(student) {
+        let score = 0;
+        score += student.hasShadow ? 4 : 0;
+        score += student.paleComplexion ? 3 : 0;
+        score += student.allergicToGarlic ? 3 : 0;
+
+        return score > 6;
+    }
+
+    randomDecision() {
+        return Math.random() < 0.5;
+    }
+
+    //TODO Implement decisionTree method
+}
